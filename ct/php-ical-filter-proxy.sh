b70a3eab8d584ad14a91ea245283428c33c311d5
Steven Bühner <buehner@me.com>
2026-04-30 13:20:43 +0200

    Fehlermeldung in Shellcheck

--- ct/php-ical-filter-proxy.sh
+++ ct/php-ical-filter-proxy.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+# shellcheck source=/dev/null
 source <(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/build.func)
 # Copyright (c) 2021-2026 Steven Buehner
 # Author: Steven Buehner
